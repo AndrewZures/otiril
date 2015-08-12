76fe86e674dc5fb84058fd50665d8f2605b3544b
Andrew Zures <azures@8thlight.com>
2015-08-11 20:51:45 -0500

    jest tests working

--- spec/sum-test.js
+++ spec/sum-test.js
@@ -1,9 +1,8 @@
 //__tests__/sum-test.js
-jest.dontMock('../sum');
+// jest.dontMock('../sum');
 
 describe('sum', function() {
   it('adds 1 + 2 to equal 3', function() {
-    var sum = require('../sum');
-    expect(sum(1, 2)).toBe(3);
+    expect(1+2).toBe(3);
   });
 });
